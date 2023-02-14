1ea7b542228392d8eb2cd163571767e1e1c19129
Jean <acalto2@gmail.com>
2023-02-14 23:22:41 +0100

    26

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'counter.dart';
-import 'show_me_counter.dart';
+import 'pages/chgnotiprov_chgnotiproxyprov.dart';
+import 'pages/chgnotiprov_proxyprov.dart';
+import 'pages/proxyprov_create_update.dart';
+import 'pages/proxyprov_proxyprov.dart';
+import 'pages/proxyprov_update.dart';
+import 'pages/why_proxyprov.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final Counter _counter = Counter();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Anonymous Route',
+      title: 'ProxyProvider Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case '/':
-            return MaterialPageRoute(
-              builder: (context) => ChangeNotifierProvider.value(
-                value: _counter,
-                child: MyHomePage(),
-              ),
-            );
-
-          case '/counter':
-            return MaterialPageRoute(
-              builder: (context) => ChangeNotifierProvider.value(
-                value: _counter,
-                child: ShowMeCounter(),
-              ),
-            );
-
-          default:
-            return null;
-        }
-      },
+      home: const MyHomePage(),
     );
-  }
-
-  @override
-  void dispose() {
-    _counter.dispose();
-    super.dispose();
   }
 }
 
@@ -65,29 +34,96 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text(
-                'Show Me Counter',
-                style: TextStyle(fontSize: 20.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              ElevatedButton(
+                child: Text(
+                  'Why\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => WhyProxyProv(),
+                  ),
+                ),
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/counter');
-              },
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text(
-                'Increment Counter',
-                style: TextStyle(fontSize: 20.0),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'ProxyProvider\nupdate',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ProxyProvUpdate(),
+                  ),
+                ),
               ),
-              onPressed: () {
-                context.read<Counter>().increment();
-              },
-            )
-          ],
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'ProxyProvider\ncreate/update',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ProxyProvCreateUpdate(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'ProxyProvider\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ProxyProvProxyProv(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'ChangeNotifierProvider\nChangeNotifierProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ChgNotiProvChgNotiProxyProv(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text(
+                  'ChangeNotifierProvider\nProxyProvider',
+                  style: TextStyle(fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ChgNotiProvProxyProv(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

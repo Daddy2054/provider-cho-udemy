d21cc0c1571b422e197c6465b5cf51585ef3d626
Jean <acalto2@gmail.com>
2023-02-16 00:40:20 +0100

    38

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import 'app_provider.dart';
-import 'success_page.dart';
+import 'pages/todos_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,161 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<AppProvider>(
-      create: (_) => AppProvider(),
-      child: MaterialApp(
-        title: 'addListener of ChangeNotifier',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(),
+    return MaterialApp(
+      title: 'TODOS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? searchTerm;
-  late final AppProvider appProv;
-
-  @override
-  void initState() {
-    super.initState();
-    appProv = context.read<AppProvider>();
-    appProv.addListener(appListener);
-  }
-
-  void appListener() {
-    if (appProv.state == AppState.success) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const SuccessPage()));
-    } else if (appProv.state == AppState.error) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return const AlertDialog(
-            content: Text('something went wrong'),
-          );
-        },
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    appProv.removeListener(appListener);
-    super.dispose();
-  }
-
-  void submit() async {
-    setState(() {
-      autovalidateMode = AutovalidateMode.always;
-    });
-    final form = formKey.currentState;
-    if (form == null || !form.validate()) {
-      return;
-    }
-    form.save();
-
-    //try {
-    context.read<AppProvider>().getResult(searchTerm!);
-    //   Navigator.push(
-    //       context,
-    //       MaterialPageRoute(
-    //         builder: (context) => SuccessPage(),
-    //       ));
-    // //} catch (e) {
-    //   showDialog(
-    //     context: context,
-    //     builder: (context) {
-    //       return AlertDialog(
-    //         content: Text('something went wrong'),
-    //       );
-    //     },
-    //   );
-    // //}
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final appState = context.watch<AppProvider>().state;
-
-    // if (appState == AppState.success) {
-    //   WidgetsBinding.instance.addPostFrameCallback((_) {
-    //     Navigator.push(
-    //         context,
-    //         MaterialPageRoute(
-    //           builder: (context) => SuccessPage(),
-    //         ));
-    //   });
-    // } else if (appState == AppState.error) {
-    //   WidgetsBinding.instance.addPostFrameCallback((_) {
-    //     showDialog(
-    //       context: context,
-    //       builder: (context) {
-    //         return AlertDialog(
-    //           content: Text('something went wrong'),
-    //         );
-    //       },
-    //     );
-    //   });
-    // }
-
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Form(
-              key: formKey,
-              autovalidateMode: autovalidateMode,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  TextFormField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text('Search'),
-                      prefixIcon: Icon(Icons.search),
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return 'Search term required';
-                      }
-                      return null;
-                    },
-                    onSaved: (String? value) {
-                      searchTerm = value;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  ElevatedButton(
-                    child: Text(
-                      appState == AppState.loading
-                          ? 'Loading...'
-                          : 'Get Result',
-                      style: TextStyle(fontSize: 24.0),
-                    ),
-                    onPressed: appState == AppState.loading ? null : submit,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: const TodosPage(),
     );
   }
 }

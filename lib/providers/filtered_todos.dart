6c8341bd3a74b8402b4dd8cd719e3f70551cdd37
Jean <acalto2@gmail.com>
2023-02-18 00:02:33 +0100

    44

---
+++ lib/providers/filtered_todos.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider_cho_udemy/models/todo_model.dart';
+import 'package:provider_cho_udemy/providers/todo_filter.dart';
+import 'package:provider_cho_udemy/providers/todo_list.dart';
+import 'package:provider_cho_udemy/providers/todo_search.dart';
+
+class FilteredTodosState extends Equatable {
+  final List<Todo> filteredTodos;
+  FilteredTodosState({
+    required this.filteredTodos,
+  });
+
+  factory FilteredTodosState.initial() => FilteredTodosState(filteredTodos: []);
+
+  @override
+  List<Object> get props => [filteredTodos];
+
+  @override
+  bool get stringify => true;
+
+  FilteredTodosState copyWith({
+    List<Todo>? filteredTodos,
+  }) {
+    return FilteredTodosState(
+      filteredTodos: filteredTodos ?? this.filteredTodos,
+    );
+  }
+}
+
+class FilteredTodos with ChangeNotifier {
+  FilteredTodosState _state = FilteredTodosState.initial();
+
+  FilteredTodosState get state => _state;
+
+  void update(
+    TodoList todoList,
+    TodoFilter todoFilter,
+    TodoSearch todoSearch,
+  ) {
+    List<Todo> _filteredTodos;
+    switch (todoFilter.state.filter) {
+      case Filter.active:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => !todo.completed).toList();
+        break;
+      case Filter.completed:
+        _filteredTodos =
+            todoList.state.todos.where((Todo todo) => todo.completed).toList();
+        break;
+
+      case Filter.all:
+      default:
+        _filteredTodos = todoList.state.todos;
+        break;
+    }
+    if (todoSearch.state.searchTerm.isNotEmpty) {
+      _filteredTodos = _filteredTodos.where((Todo todo) {
+        return todo.desc.toLowerCase().contains(
+              todoSearch.state.searchTerm,
+            );
+      }).toList();
+    }
+
+    _state = _state.copyWith(
+      filteredTodos: _filteredTodos,
+    );
+    notifyListeners();
+  }
+}

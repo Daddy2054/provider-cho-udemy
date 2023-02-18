660ffa765e713b01b1a3b577d218f5c6ba4de286
Jean <acalto2@gmail.com>
2023-02-19 00:14:17 +0100

    48

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_cho_udemy/models/todo_model.dart';
+import 'package:provider_cho_udemy/providers/todo_filter.dart';
 import 'package:provider_cho_udemy/providers/todo_list.dart';
 
 import '../providers/active_todo_count.dart';
+import '../providers/todo_search.dart';
 
 class TodosPage extends StatefulWidget {
   const TodosPage({Key? key}) : super(key: key);
@@ -27,12 +30,71 @@ class _TodosPageState extends State<TodosPage> {
 //                              Text('TODOS'),
                 const TodoHeader(),
                 CreateTodo(),
+                const SizedBox(height: 20),
+                SearchAndFilterTodo(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class SearchAndFilterTodo extends StatelessWidget {
+  const SearchAndFilterTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Search todos',
+            border: InputBorder.none,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          onChanged: (String? newSearchTerm) {
+            context.read<TodoSearch>().setSearchTerm(newSearchTerm!);
+          },
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            filterButton(context, Filter.all),
+            filterButton(context, Filter.active),
+            filterButton(context, Filter.completed),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget filterButton(BuildContext context, Filter filter) {
+    return TextButton(
+      onPressed: () {
+        context.read<TodoFilter>().changeFilter(filter);
+      },
+      child: Text(
+        filter == Filter.all
+            ? 'All'
+            : filter == Filter.active
+                ? 'Active'
+                : 'Completed',
+        style: TextStyle(
+          fontSize: 18,
+          color: textColor(context, filter),
+        ),
+      ),
+    );
+  }
+
+  Color textColor(BuildContext context, Filter filter) {
+    return context.watch<TodoFilter>().state.filter == filter
+        ? Colors.blue
+        : Colors.grey;
   }
 }
 

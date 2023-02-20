cc2c0935cde86a71d12fe1b696b9b14f49d3a999
Jean <acalto2@gmail.com>
2023-02-20 22:00:28 +0100

    56

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'pages/todos_page.dart';
-import 'providers/providers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,45 +9,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<TodoList>(create: (context) => TodoList()),
-        ChangeNotifierProvider<TodoFilter>(create: (context) => TodoFilter()),
-        ChangeNotifierProvider<TodoSearch>(create: (context) => TodoSearch()),
-        ProxyProvider<TodoList, ActiveTodoCount>(
-          update: (
-            BuildContext context,
-            TodoList todoList,
-            ActiveTodoCount? _,
-          ) {
-            return ActiveTodoCount(
-              todoList: todoList,
-            );
-          },
+    return MaterialApp(
+      title: 'StateNotifier',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StateNotifier'),
+      ),
+      body: Center(
+        child: Text(
+          '0',
+          style: Theme.of(context).textTheme.headline2,
         ),
-        ProxyProvider3<TodoList, TodoFilter, TodoSearch, FilteredTodos>(
-          update: (
-            BuildContext context,
-            TodoList todoList,
-            TodoFilter todoFilter,
-            TodoSearch todoSearch,
-            FilteredTodos? _,
-          ) {
-            return FilteredTodos(
-              todoList: todoList,
-              todoFilter: todoFilter,
-              todoSearch: todoSearch,
-            );
-          },
-        ),
-      ],
-      child: MaterialApp(
-        title: 'TODOS',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const TodosPage(),
       ),
     );
   }

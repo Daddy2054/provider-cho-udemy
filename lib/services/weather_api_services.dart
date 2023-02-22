bc12956707d7b31de95cf8c2b4a00b5eab098e3a
Jean <acalto2@gmail.com>
2023-02-22 22:11:50 +0100

    68

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -5,6 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:open_weather_provider/exeptions/weather_exeption.dart';
 import 'package:open_weather_provider/models/direct_geocoding.dart';
+import 'package:open_weather_provider/models/weather.dart';
 
 import '../constants/constants.dart';
 import 'http_error_handler.dart';
@@ -38,6 +39,33 @@ class WeatherApiServices {
       }
       final directGeocoding = DirectGeocoding.fromJson(responseBody);
       return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(DirectGeocoding directGeocoding) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kApiHost,
+      path: '/data/2.5/weather',
+      queryParameters: {
+        'lat': '$directGeocoding.lat}',
+        'lon': '$directGeocoding.lon}',
+        'units': kUnit,
+        'appid': dotenv.env['APPID'],
+      },
+    );
+
+    try {
+      final http.Response response = await httpClient.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+
+      final weatherJson = json.decode(response.body);
+      final Weather weather = Weather.fromJson(weatherJson);
+      return weather;
     } catch (e) {
       rethrow;
     }
